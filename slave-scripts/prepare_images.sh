8077c3dfaf7cabdc041c1c3580009c8db6dda9ff
Evgeny Sikachev <esikachev@mirantis.com>
2016-05-05 15:58:20 +0300

    Fix scripts for uploading images

--- slave-scripts/prepare_images.sh
+++ slave-scripts/prepare_images.sh
@@ -2,5 +2,5 @@
 
 . $FUNCTION_PATH/functions.sh
 
-ssh 172.18.79.150 -l qa 'scp -r mitaka root@172.18.79.154:'
+ssh 172.18.79.150 -l qa 'sshpass -p r00tme scp -o StrictHostKeyChecking=no -r mitaka root@172.18.79.154:'
 upload_images 172.18.79.154 root r00tme
