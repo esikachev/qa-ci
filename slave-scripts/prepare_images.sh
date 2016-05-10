32e508004c17e6a6498b98ad261a3e84ea344d2b
Evgeny Sikachev <esikachev@mirantis.com>
2016-05-10 14:21:09 +0300

    Added removing of hosts from known_hosts

--- slave-scripts/prepare_images.sh
+++ slave-scripts/prepare_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . $FUNCTION_PATH/functions.sh
-
+ssh-keygen -R 172.18.79.154
 ssh 172.18.79.150 -l qa 'sshpass -p r00tme scp -o StrictHostKeyChecking=no -r mitaka root@172.18.79.154:'
 upload_images 172.18.79.154 root r00tme
