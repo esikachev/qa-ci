a281775f4469cc3a7fedece94ecc82bcd2b6f83d
Alina Nesterova <zemuvier@gmail.com>
2016-06-08 18:07:33 +0300

    Added script for ambari image
    Added automatic create of cluster template with ambari image
    
    Change-Id: Id679b37a99f48f1380df2874d7a256ec9b64efaa

--- slave-scripts/sahara_tests.sh
+++ slave-scripts/sahara_tests.sh
@@ -12,10 +12,16 @@ plugin=$(echo $JOB_NAME | awk -F '-' '{ print $3 }')
 case $plugin in
     ambari_2.3)
        template_image_prefix="ambari_2_1"
+       nova boot --image mirror --flavor m1.small --security-groups default mirror
+       floating_ip=$(nova floating-ip-create | awk '{ print $4 }')
+       nova floating-ip-associate mirror floating_ip
+       hdp=http://$floating_ip/hdp/centos6/2.x/updates/2.3.4.7/
+       hdp_utils=http://$floating_ip/hdp-utils/repos/centos6/
+       sed -i '/cluster_configs:/a \ \ \ \ \ \ \ \ \general:\n \ \ \ \ \ \ \ \ \ \HDP: $hdp\n \ \ \ \ \ \ \ \ \ \HDP-UTILS: $hdp_utils' $SAHARA_TESTS_PATH/sahara_tests/scenario/defaults/ambari-2.3.yaml.mako
        ;;
     vanilla_2.7.1)
        template_image_prefix="vanilla_two_seven_one"
-    ;;
+       ;;
     transient)
        plugin=vanilla_2.7.1
        template_image_prefix="vanilla_two_seven_one"
