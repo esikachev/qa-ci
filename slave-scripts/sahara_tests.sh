6f926a71a15ab751d64f224c5752285881dd31c8
Alina Nesterova <zemuvier@gmail.com>
2016-06-09 12:28:17 +0300

    Update security information
    Add credentials taken from Jenkins config file
    
    Change-Id: I66672d5407f252dcbf0019f87f5d083fd98664c0

--- slave-scripts/sahara_tests.sh
+++ slave-scripts/sahara_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 . $FUNCTION_PATH/functions.sh
-
+. /home/jenkins/ci_openrc
 export SAHARA_TESTS_PATH=$WORKSPACE/sahara-tests
 get_dependency "$SAHARA_TESTS_PATH" "openstack/sahara-tests" "master"
 template_vars_file=$WORKSPACE/sahara-tests/template_vars.ini
@@ -12,13 +12,13 @@ plugin=$(echo $JOB_NAME | awk -F '-' '{ print $3 }')
 case $plugin in
     ambari_2.3)
        template_image_prefix="ambari_2_1"
-       nova boot --image mirror --flavor m1.small --security-groups default --nic net-id=2c908c7f-4139-4d25-9a75-daea4ed9b603 mirror
-       ip=$(nova show mirror | grep 'admin_internal_net network' | awk '{print$5}')
-       port=$(neutron port-list | grep $ip | awk '{print$2}')
-       neutron floatingip-create --port-id $port admin_floating_net
-       hdp=http://$floating_ip/hdp/centos6/2.x/updates/2.3.4.7/
-       hdp_utils=http://$floating_ip/hdp-utils/repos/centos6/
-       sed -i '/cluster_configs:/a \ \ \ \ \ \ \ \ \general:\n \ \ \ \ \ \ \ \ \ \HDP: $hdp\n \ \ \ \ \ \ \ \ \ \HDP-UTILS: $hdp_utils' $SAHARA_TESTS_PATH/sahara_tests/scenario/defaults/ambari-2.3.yaml.mako
+       sshpass -p $controller_password ssh $controller_username@$controller_ip ". openrc && nova boot --image mirror --flavor m1.small --security-groups default --nic net-id=2c908c7f-4139-4d25-9a75-daea4ed9b603 mirror"
+       ip=$(sshpass -p $controller_password ssh $controller_username@$controller_ip ". openrc && nova show mirror | grep 'admin_internal_net network' | awk '{print\$5}'")
+       port=$(sshpass -p $controller_password ssh $controller_username@$controller_ip ". openrc && neutron port-list | grep $ip | awk '{print\$2}'")
+       sshpass -p $controller_password ssh $controller_username@$controller_ip ". openrc && neutron floatingip-create --port-id $port admin_floating_net"
+       hdp=http://$ip/hdp/centos6/2.x/updates/2.3.4.7/
+       hdp_utils=http://$ip/hdp-utils/repos/centos6/
+       sed -i '/cluster_configs:/a \ \ \ \ \ \ \ \ \general:\n \ \ \ \ \ \ \ \ \ \ \\HDP: $hdp\n \ \ \ \ \ \ \ \ \ \HDP-UTILS: $hdp_utils' $SAHARA_TESTS_PATH/sahara_tests/scenario/defaults/ambari-2.3.yaml.mako
        ;;
     vanilla_2.7.1)
        template_image_prefix="vanilla_two_seven_one"
