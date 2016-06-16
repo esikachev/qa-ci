8f0962c2734f564a5459f1d680c9fc61ccfd888d
Evgeny Sikachev <esikachev@mirantis.com>
2016-06-16 18:25:46 +0300

    Fixed command on host in prepare_images
    ' -> "
    
    Change-Id: I9679bfb0b7390617814061628f429a2890b0dadb

--- slave-scripts/prepare_images.sh
+++ slave-scripts/prepare_images.sh
@@ -4,5 +4,5 @@
 
 . $FUNCTION_PATH/functions.sh
 ssh-keygen -R $controller_ip
-ssh $host_ip -l $host_username 'sshpass -p $controller_password scp -o StrictHostKeyChecking=no -r mitaka $controller_username@$controller_ip:'
+ssh $host_ip -l $host_username "sshpass -p $controller_password scp -o StrictHostKeyChecking=no -r mitaka $controller_username@$controller_ip:"
 upload_images $controller_ip $controller_username $controller_password
